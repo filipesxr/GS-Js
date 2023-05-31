4b7b120b9c7a6759c372552a0814ebba9d8fcbc5
filipesxr <rm97830@fiap.com.br>
2023-05-31 17:39:56 -0300

    Carrossel de Imagens

--- js/login.js
+++ js/login.js
@@ -1,11 +1,12 @@
 // Olá
 alert("Seja bem-vindo!")
+
 // Definindo cores
 const bodyelement = document.querySelector("body");
 bodyelement.style.backgroundColor = "#2F4F4F";
 
 const formelement = document.querySelector(".form");
-formelement.style.backgroundColor = "#FFFFFF"
+formelement.style.backgroundColor = "#FFFFFF";
 
 const loginelement = document.querySelector(".login");
 loginelement.style.backgroundColor = "#FFFFFF";
@@ -26,8 +27,8 @@ const buttonenter = document.querySelector(".enter");
 
 buttonenter.addEventListener("click", ()=>{
 if(inputuser.value == "Admin" && inputpassword.value == "123456"){
-    alert("Log In autenticado!")
-    window.location.href = "./principal.html"
+    alert("Log In autenticado!");
+    window.location.href = "https://www.unicef.org/brazil/comunicados-de-imprensa/relatorio-da-onu-numeros-globais-de-fome-subiram-para-cerca-de-828-milhoes-em-2021";
 }
 else{
     alert("Log In inválido!");
@@ -42,6 +43,17 @@ const buttonsubmit = document.querySelector(".submit");
 
 buttonsubmit.addEventListener("click", ()=>{
     if(inputname.value.length == 0 || inputemail.value.length == 0 || inputnumber.value.length == 0){
-        alert("Preencha todos os campos!")
+        alert("Preencha todos os campos!");
+    }
+    if(inputname.value.length < 4){
+        alert("O nome deve ter pelo menos 4 carácteres!")
+    }
+    if(inputemail.value.length < 4){
+        alert("O email deve ter pelo menos 4 carácteres!")
+    }
+    if(inputnumber.value.length < 11){
+        alert("O número de telefone deve ter pelo menos 11 carácteres!")
     }
 })
+
+// Imagens e Carrossel
